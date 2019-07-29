a552946a45e0aa02cc6251a5329bb0d5e55f867a
Aristides A. Morcillo <aristides03_30@hotmail.com>
2019-07-28 22:36:06 -0500

    se realiza template del header

--- AppDS9/src/app/components/App.vue
+++ AppDS9/src/app/components/App.vue
@@ -1,5 +1,39 @@
 <template>
-   <nav class="navbar navbar-light bg-dark">
-      <a href="/" class="navbar-b">LOGO AQUI</a>
-   </nav> 
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+         <img src="/img/logo2.png" width="35" height="35" class="d-inline-block align-top" alt="">
+              Colaborative Network</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+            <a class="nav-link" href="#">Servicios <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Quien Somos</a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+               <a class="dropdown-item" href="#">Action</a>
+               <a class="dropdown-item" href="#">Another action</a>
+               <div class="dropdown-divider"></div>
+               <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+            </li>
+            <li class="nav-item">
+            
+            </li>
+         </ul>
+         <form class="form-inline my-2 my-lg-0">
+            
+            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Login</button>
+         </form>
+      </div>
+   </nav>
 </template>
